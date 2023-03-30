ac9d787f8b2fc400162b190e059b8ca6941b7b16
ahmetgml <129423438+ahmetgml@users.noreply.github.com>
2023-03-30 20:48:46 +0300

    Plugin1+2

---
+++ TestPlugins.Plugin1/Plugin1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TestPlugins.PluginInterface;
+
+namespace TestPlugins.Plugin1
+{
+    public class Plugin1 : IPlugin
+    {
+        public void Init(IServiceCollection services)
+        {
+            Console.WriteLine("Plugin1 Initing.");
+
+            services.AddTransient<IService1, Service1>();
+
+            Console.WriteLine("Plugin1 Inited.");
+        }
+
+        public void Run(IServiceProvider serviceProvider)
+        {
+            var service1 = serviceProvider.GetRequiredService<Service1>();
+        }
+    }
+
+    public interface IService1
+    { }
+
+    public class Service1 : IService1
+    {
+        public Service1()
+        {
+            Console.WriteLine("Service1 Ctor.");
+        }
+    }
+}
---
+++ TestPlugins.Plugin2/Plugin2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TestPlugins.PluginInterface;
+
+namespace TestPlugins.Plugin2
+{
+    public class Plugin2 : IPlugin
+    {
+        public void Init(IServiceCollection services)
+        {
+            Console.WriteLine("Plugin2 Initing.");
+
+            services.AddTransient<IService2, Service2>();
+
+            Console.WriteLine("Plugin2 Inited.");
+        }
+
+        public void Run(IServiceProvider serviceProvider)
+        {
+            var service2 = serviceProvider.GetRequiredService<Service2>();
+        }
+    }
+
+    public interface IService2
+    { }
+
+    public class Service2 : IService2
+    {
+        public Service2()
+        {
+            Console.WriteLine("Service2 Ctor.");
+        }
+    }
+}
